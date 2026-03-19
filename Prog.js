447a97cb58241827a437512125677aab37f724c7
raginimegala <ragini.megala@gmail.com>
2026-03-19 17:35:31 +0530

    Youtube_clone_initial commit

---
+++ Prog.js
@@ -0,0 +1,16 @@
+let a=10;
+let b="cat";
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a, " ", b);
+
+
+console.log( 0 == "0" )
+console.log( 0 === "0" )
+
+import a from "./index.js";
+console.log(a);
---
+++ index.js
@@ -0,0 +1,14 @@
+let a=10;
+let b=20;
+let c=a+b;
+
+console.log(c);
+
+// alert("rag");
+// prompt("ini");
+
+const hi={a:10, c:40}
+document.write(hi.a);
+console.log(hi)
+
+export let a = "ragini";
